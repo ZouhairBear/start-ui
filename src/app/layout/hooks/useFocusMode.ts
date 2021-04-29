7e797340cb06d55e36ce5ae356329c5e213d8870
ZouhairBear <60467819+ZouhairBear@users.noreply.github.com>
2021-04-29 13:21:51 +0200

    Initial commit

---
+++ src/app/layout/hooks/useFocusMode.ts
@@ -0,0 +1,14 @@
+import { useContext, useEffect } from 'react';
+
+import { LayoutContext } from '@/app/layout';
+
+export const useFocusMode = (enabled = true) => {
+  const ctx = useContext(LayoutContext);
+  const { setIsFocusMode } = ctx || {};
+
+  useEffect(() => {
+    if (!ctx) return () => {};
+    setIsFocusMode(enabled);
+    return () => setIsFocusMode(false);
+  }, [ctx, setIsFocusMode, enabled]);
+};
